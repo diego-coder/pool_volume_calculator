386015d0a56d1e96eab2df4893581db3ebc14061
diego-coder <thediegocoder@gmail.com>
2020-05-22 14:09:34 -0700

    first commit

---
+++ volume_of_pool_water_calculator/pool_calculator.py
@@ -0,0 +1,14 @@
+# Program calculates the volume of water it takes to fill a pool.
+
+length_of_pool = 10
+width_of_pool = 5
+depth_of_pool = 5
+
+gallons_used = length_of_pool * width_of_pool * depth_of_pool * 7.5
+
+cost_per_gallon = .01
+
+water_cost = cost_per_gallon * volume_in_gallons
+
+gallons_used
+water_cost
