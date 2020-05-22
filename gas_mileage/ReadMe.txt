f5e532a2728f2e8ef17904d9d9572be970c40b25
diego-coder <thediegocoder@gmail.com>
2020-05-21 21:25:52 -0700

    first commit

---
+++ gas_mileage/ReadMe.txt
@@ -0,0 +1,3 @@
+ReadMe.txt
+
+This python3 program calculates the average miles per gallon I get while driving.
---
+++ gas_mileage/gas_mileage.py
@@ -0,0 +1,6 @@
+miles_traveled = 98
+gallons_fuel = 1.7
+
+miles_per_gallon = miles_traveled / gallons_fuel
+
+print(miles_per_gallon)
